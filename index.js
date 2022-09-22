9cb842eb72178ec01033838a98c740c0fe9edfc8
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2022-09-22 21:45:19 +0530

    feat: added code to check denomination

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cashGiven=document.querySelector("#cash-given")
 const checkButton=document.querySelector("#check-button");
 
 const message=document.querySelector("#error-message");
+const noOfNotes= document.querySelectorAll(".no-of-notes");
 
+const availableNotes=[2000,500,100,20,10,5,1];
 
 checkButton.addEventListener("click",
 function validateBillandCashAmount(){
@@ -15,14 +17,19 @@ function validateBillandCashAmount(){
             calculateChange(ammountToBeReturned);
         }else{
 
-            showMessage("Inadequate Cash Given");
+            showMessage("Wanna wash Plate!! ");
         }
     }else{
        showMessage("Invalid Bill Amount")
     }
 });
-function calculateChange(){
-
+function calculateChange(amountToBeReturned){
+    for(let i=0;i<availableNotes.length;i++){
+        const numberOfNotes=Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned%=availableNotes[i];
+        noOfNotes[i].innerText=numberOfNotes;
+         
+    }
 }
 
 function showMessage(msg){
